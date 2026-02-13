176c53983821ef00bccb16f4a28d69d9c2a3e381
Deepali <deepaggarwall2004@gmail.com>
2026-02-13 16:00:41 +0530

    Feat : Igniting our app

--- Code/App.js
+++ Code/App.js
@@ -1,12 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
-    "div", { id: "parent"}, React.createElement(
-        "div" , { id : " child"} , React.createElement("h1" , {} , "I am H1 tag")
+    "div",
+     { id: "parent"}, 
+    React.createElement(
+        "div" ,
+        { id : " child"} ,
+        [React.createElement("h1" , {} , "I am H1 tag") , 
+        React.createElement("h2", {} , "I am H2 tag")],
     )
 );
-
-
-
-
 
 const heading = React.createElement(
     "h1",
---
+++ Code/README.md
@@ -0,0 +1,10 @@
+...
+# Namaste React 
+
+...
+
+# Parcel 
+-Dev Build
+-Local Server
+- HMR = hot module replacement
+- File Watching Algorithm - written in C++
