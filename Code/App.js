56d1a8ccaaf352b650a7bbe6c6d8235232b6242f
Deepali <deepaggarwall2004@gmail.com>
2026-02-14 00:41:10 +0530

    Basic of React

--- Code/App.js
+++ Code/App.js
@@ -1,23 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-     { id: "parent"}, 
-    React.createElement(
-        "div" ,
-        { id : " child"} ,
-        [React.createElement("h1" , {} , "I am H1 tag") , 
-        React.createElement("h2", {} , "I am H2 tag")],
-    )
+/* react element
+ const parent = React.createElement("div",{ id: "parent"}, React.createElement("div" ,{ id : " child"} ,[React.createElement("h1" , {} , "I am H1 tag") , React.createElement("h2", {} , "I am H2 tag")],
+ )
+ );
+const heading = React.createElement("h1",{ id : "heading" , xyz: "abc"}, "Hello World From react");console.log(heading);
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent) */
+
+//JSX
+//JSX (TRANSPILED before it reaches the JS) - parcel
+//JSX => BABEL Transpiles it to React.createElement => ReactElement.JS object => HTMLelement(render)
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste react using JSX    
+    </h1>
 );
 
-const heading = React.createElement(
-    "h1",
-    { id : "heading" , xyz: "abc"}, 
-    "Hello World From react");
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <Title></Title>
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
 
-  console.log(heading);  
-    
+// React Component
+// class based component - old
+// functional component - new
+//REACT Function component - 
+// const HeadingComponent = () => {
+//     return <h1>Namaste React Functional Component</h1>
+// };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(<HeadingComponent />);
