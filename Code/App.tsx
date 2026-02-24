1aad6b50e3b268a81a32933c61d12e6b85993b7f
Deepali <deepaggarwall2004@gmail.com>
2026-02-25 01:15:00 +0530

    feat: add restaurant list rendering with typed props

--- Code/App.tsx
+++ Code/App.tsx
@@ -19,11 +19,597 @@ const Header = () => {
     );
 };
 
+// const styleCard = {
+//     backgroundColor: "#f0f0f0"
+// }
+
+type RestaurantData = {
+    id: string,
+    name: string;
+    cuisines: string[];
+    avgRating: number;
+    costForTwo: string;
+    cloudinaryImageId: string;
+    sla: {
+        deliveryTime: number;
+    };
+}
+type RestaurantCardProps = {
+    resData: RestaurantData;
+};
+type RestaurantApiItem = {
+  info: RestaurantData;
+};
+
+const RestaurantCard = ({resData} : RestaurantCardProps) => {
+    const {
+        cloudinaryImageId, 
+        name, 
+        cuisines,
+        avgRating, 
+        costForTwo, 
+        sla: {deliveryTime}
+    } = resData;
+    return (
+        <div className="res-card">
+            <img 
+            className="res-logo"
+                alt={name}
+                src={ "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
+            />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{deliveryTime} mins</h4>
+        </div>
+    )
+}
+
+
+
+const resList: RestaurantApiItem[] =[
+    {
+        info: {
+        "id": "424877",
+        "name": "Dilliwale Pahelvanjee'S Cholle Bhature",
+        "cloudinaryImageId": "tajbfx84hahoum5w9niz",
+        "locality": "Moti Begumwadi",
+        "areaName": "New Textile Market",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+            "Punjabi",
+            "Street Food",
+            "Fast Food"
+        ],
+        "avgRating": 4.7,
+        "veg": true,
+        "parentId": "289667",
+        "avgRatingString": "4.7",
+        "totalRatingsString": "10K+",
+        "sla": {
+            "deliveryTime": 23,
+            "lastMileTravel": 3,
+            "serviceability": "SERVICEABLE",
+            "slaString": "20-25 mins",
+            "lastMileTravelString": "3.0 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-25 18:15:00",
+            "opened": true
+        },
+        "isOpen": true,
+        "type": "F",
+        "aggregatedDiscountInfoV3": {
+            "header": "₹50 OFF",
+            "subHeader": "ABOVE ₹499",
+            "discountTag": "FLAT DEAL"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/dilliwale-pahelvanjees-cholle-bhature-moti-begumwadi-new-textile-market-rest424877",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "205488",
+        "name": "Jay Jalaram Thali",
+        "cloudinaryImageId": "56d5abe62452d827b7480c4f5515460a",
+        "locality": "Gopipura",
+        "areaName": "Gopipura",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+            "Thalis",
+            "Gujarati",
+            "Punjabi",
+            "Chinese",
+            "Biryani"
+        ],
+        "avgRating": 4,
+        "parentId": "110113",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "25K+",
+        "sla": {
+            "deliveryTime": 26,
+            "lastMileTravel": 3.6,
+            "serviceability": "SERVICEABLE",
+            "slaString": "25-30 mins",
+            "lastMileTravelString": "3.6 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-25 16:00:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+            {
+                "imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+                "description": "OnlyOnSwiggy"
+            }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+            "imageBased": {
+                "badgeObject": [
+                {
+                    "attributes": {
+                    "description": "OnlyOnSwiggy",
+                    "imageId": "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png"
+                    }
+                }
+                ]
+            }
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹19"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/jay-jalaram-thali-gopipura-rest205488",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "456090",
+        "name": "Mahalaxmi Juice And Fast Food Corner",
+        "cloudinaryImageId": "zklivmqduux38pw9ikax",
+        "locality": "Adajan Patiya",
+        "areaName": "Adajan Patiya",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+            "Ice Cream",
+            "Burgers",
+            "Pizzas",
+            "Salads",
+            "Beverages"
+        ],
+        "avgRating": 4.7,
+        "veg": true,
+        "parentId": "273935",
+        "avgRatingString": "4.7",
+        "totalRatingsString": "26K+",
+        "sla": {
+            "deliveryTime": 24,
+            "lastMileTravel": 5.7,
+            "serviceability": "SERVICEABLE",
+            "slaString": "20-25 mins",
+            "lastMileTravelString": "5.7 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-26 00:00:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+            {
+                "imageId": "Ratnesh_Badges/Rx_Awards_2025/_Milestone%20Restaurants.png",
+                "description": "Delivery!"
+            }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+            "imageBased": {
+                "badgeObject": [
+                {
+                    "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Ratnesh_Badges/Rx_Awards_2025/_Milestone%20Restaurants.png"
+                    }
+                }
+                ]
+            }
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹109"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "4.0",
+            "ratingCount": "377"
+            },
+            "source": "GOOGLE",
+            "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/mahalaxmi-juice-and-fast-food-corner-adajan-patiya-rest456090",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "841660",
+        "name": "Domino's Pizza",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/29d4c993-4ea2-4337-8e2a-fb8b348cdec3_841660.JPG",
+        "locality": "UDHANA DARWAZA",
+        "areaName": "SHHLOK BUSINESS CENTRE",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+            "Pizzas",
+            "Italian",
+            "Pastas",
+            "Desserts"
+        ],
+        "avgRating": 4.3,
+        "veg": true,
+        "parentId": "2456",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "896",
+        "sla": {
+            "deliveryTime": 25,
+            "lastMileTravel": 2.8,
+            "serviceability": "SERVICEABLE",
+            "slaString": "20-25 mins",
+            "lastMileTravelString": "2.8 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-25 23:59:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+            {
+                "imageId": "Rxawards/_CATEGORY-Pizza.png",
+                "description": "Delivery!"
+            },
+            {
+                "imageId": "v1695133679/badges/Pure_Veg111.png",
+                "description": "pureveg"
+            }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+            "imageBased": {
+                "badgeObject": [
+                {
+                    "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Pizza.png"
+                    }
+                },
+                {
+                    "attributes": {
+                    "description": "pureveg",
+                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                    }
+                }
+                ]
+            }
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹59"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/dominos-pizza-udhana-darwaza-shhlok-business-centre-rest841660",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "100721",
+        "name": "McDonald's",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/3/d46dbd39-f4b4-414b-a753-c6ded6705cad_100721.JPG",
+        "locality": "Rustampura",
+        "areaName": "Rustampura",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+            "Burgers",
+            "Beverages",
+            "Cafe",
+            "Desserts"
+        ],
+        "avgRating": 4.5,
+        "parentId": "630",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "13K+",
+        "sla": {
+            "deliveryTime": 20,
+            "lastMileTravel": 2.8,
+            "serviceability": "SERVICEABLE",
+            "slaString": "15-20 mins",
+            "lastMileTravelString": "2.8 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-25 23:59:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+            {
+                "imageId": "android/static-assets/icons/big_rx.png",
+                "description": "bolt!"
+            }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+            "imageBased": {
+                "badgeObject": [
+                {
+                    "attributes": {
+                    "description": "bolt!",
+                    "imageId": "android/static-assets/icons/big_rx.png"
+                    }
+                }
+                ]
+            }
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹119"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "4.4",
+            "ratingCount": "4.8K+"
+            },
+            "source": "GOOGLE",
+            "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/mcdonalds-rustampura-rest100721",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "236083",
+        "name": "KFC",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/54b1c190-a0de-4b6b-bcbb-036237258ea5_236083.JPG",
+        "locality": "LP Savani Road",
+        "areaName": "Adajan Gam",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+            "Burgers",
+            "Fast Food",
+            "Rolls & Wraps"
+        ],
+        "avgRating": 4.2,
+        "parentId": "547",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "9.0K+",
+        "sla": {
+            "deliveryTime": 25,
+            "lastMileTravel": 6.8,
+            "serviceability": "SERVICEABLE",
+            "slaString": "25-30 mins",
+            "lastMileTravelString": "6.8 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-26 03:00:00",
+            "opened": true
+        },
+        "isOpen": true,
+        "type": "F",
+        "aggregatedDiscountInfoV3": {
+            "header": "40% OFF",
+            "subHeader": "UPTO ₹80"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/kfc-lp-savani-road-adajan-gam-rest236083",
+        "type": "WEBLINK"
+        }
+    },
+    {
+        info: {
+        "id": "120319",
+        "name": "Maakhan Bhog",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/23/eb87cfd7-a56c-487e-92f8-91ca2753d32b_120319.JPG",
+        "locality": "Udhna",
+        "areaName": "Ghodela House",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+            "Sweets",
+            "dessert",
+            "Snacks",
+            "Cakes & Pastries",
+            "Indian",
+            "North Indian"
+        ],
+        "avgRating": 4.4,
+        "veg": true,
+        "parentId": "6999",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "3.5K+",
+        "sla": {
+            "deliveryTime": 20,
+            "lastMileTravel": 1.5,
+            "serviceability": "SERVICEABLE",
+            "slaString": "15-20 mins",
+            "lastMileTravelString": "1.5 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2025-10-25 21:30:00",
+            "opened": true
+        },
+        "badges": {
+            "imageBadges": [
+            {
+                "imageId": "android/static-assets/icons/big_rx.png",
+                "description": "bolt!"
+            },
+            {
+                "imageId": "Rxawards/_CATEGORY-Mithai.png",
+                "description": "Delivery!"
+            }
+            ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+            "imageBased": {
+                "badgeObject": [
+                {
+                    "attributes": {
+                    "description": "bolt!",
+                    "imageId": "android/static-assets/icons/big_rx.png"
+                    }
+                },
+                {
+                    "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Mithai.png"
+                    }
+                }
+                ]
+            }
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "ITEMS",
+            "subHeader": "AT ₹99"
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT"
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "externalRatings": {
+            "aggregatedRating": {
+            "rating": "--"
+            }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+        "context": "seo-data-43bca6b8-5039-46da-935e-cdde9f114a4e"
+        },
+        "cta": {
+        "link": "https://www.swiggy.com/city/surat/maakhan-bhog-udhna-ghodela-house-rest120319",
+        "type": "WEBLINK"
+        }
+    },
+]
 const Body = () => {
     return (
         <div className="body">
             <div className="Search">Search</div>
-            <div className="res-container"></div>
+            <div className="res-container">
+                {resList.map((restaurant) => (
+                    <RestaurantCard
+                        key={restaurant.info.id}
+                        resData={restaurant.info}
+                    />
+                ))}
+            </div>  
         </div>
     )
 }
@@ -32,6 +618,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
+            <Body />
         </div>
     )
 };
