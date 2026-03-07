b752b7894de593e56047aec8e10886ce91d07c41
Deepali <deepaggarwall2004@gmail.com>
2026-03-07 14:46:44 +0530

    feat: add useState for restaurants and implement top rated filter

--- Code/src/components/Body.tsx
+++ Code/src/components/Body.tsx
@@ -1,14 +1,94 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
+type RestaurantData = {
+    info: {
+        id: string,
+        name: string;
+        cuisines: string[];
+        avgRating: number;
+        costForTwo: string;
+        cloudinaryImageId: string;
+        sla : {
+            deliveryTime: number;
+        }    
+    };    
+}
+
 const Body = () => {
+
+    // const [listofRestaurants, setlistofRestaurants] = useState<RestaurantData[]>([
+    //     {
+    //         "info": {
+    //             "id": "605415",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 4,
+    //             "deliveryTime": 43,
+            
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //             "id": "605416",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 3.9,
+    //             "deliveryTime": 43,
+            
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //             "id": "605417",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 4.6,
+    //             "deliveryTime": 43,
+            
+    //         }
+    //     },    
+    // ])
+    const [listofRestaurants, setlistofRestaurants] = useState<RestaurantData[]>(resList);
+
+
     return (
         <div className="body">
             <div className="filter">
-                <button className="filter-btn" onClick={() => {console.log("button clicked")}}>Top rated Restaurants</button>
+                <button className="filter-btn" onClick={() => {
+                        const filteredlist = listofRestaurants.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                    setlistofRestaurants(filteredlist);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
             </div>
             <div className="res-container">
-                {resList.map((restaurant) => (
+                {listofRestaurants.map((restaurant) => (
                     <RestaurantCard
                         key={restaurant.info.id}
                         resData={restaurant.info}
