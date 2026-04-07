5cc311b7a47a0e54a072f5b75bbf895b4870c0e1
Deepali <deepaggarwall2004@gmail.com>
2026-04-08 01:07:51 +0530

    feat: add UserContext and implement context API using Provider, Consumer, and useContext hook

--- Code/src/components/About.tsx
+++ Code/src/components/About.tsx
@@ -1,11 +1,36 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+import { UserClassProps } from "./UserClass";
 
-    );
-};
+class About extends React.Component<UserClassProps> {
+    constructor(props : UserClassProps) {
+        super(props);
+        console.log("Parent constructor");
+    }
+    componentDidMount(): void {
+        console.log("Parent componedidmount");
+    }
+    render(){
+        console.log("parent render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Namaste React Web Series</h2>
+                <UserClass name = {"Deepali (function)"}/>
+            </div>
+        );
+    }
 
+}
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h2>This is Namaste React Web Series</h2>
+//             <UserClass name = {"Deepali (function)"}/>
+//         </div>
+//     );
+// };
 export default About;
