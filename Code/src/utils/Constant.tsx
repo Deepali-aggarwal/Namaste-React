43ac496605789dbc13fa4933d2fbefea5a711e87
Deepali <deepaggarwall2004@gmail.com>
2026-04-08 01:08:50 +0530

    Feat: add Menu Api link

--- Code/src/utils/Constant.tsx
+++ Code/src/utils/Constant.tsx
@@ -2,3 +2,5 @@ export const LOGO_URL =
  "https://dynamic.brandcrowd.com/asset/logo/4dcc8fb9-fa3d-4acf-8bec-29cda36c7c50/logo-search-grid-2x?logoTemplateVersion=1&v=639059616780900000&layout=auto-1-1";
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const MENU_API= "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&submitAction=ENTER&restaurantId=";
