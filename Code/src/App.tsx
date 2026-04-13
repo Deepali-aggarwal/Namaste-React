aea67d17d39f22c342fdf4b004a41771d3e7dca3
Deepali <deepaggarwall2004@gmail.com>
2026-04-14 01:21:45 +0530

    Feat: added grocery section and lazy loading

--- Code/src/App.tsx
+++ Code/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {lazy , Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,6 +7,10 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Shimmer from "./components/Shimmer";
+
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 
 const AppLayout = () => {
@@ -35,6 +39,10 @@ const appRouter = createBrowserRouter([
             {
                 path: "/contact",
                 element: <Contact />
+            },
+            {
+                path: "/grocery",
+                element: <Suspense fallback={<Shimmer/>}><Grocery /></Suspense>
             },
             {
                 path: "/restaurants/:resId", //dynamically 
