8b7b756650c7892d03a1334f09b93c5493a74a77
Deepali <deepaggarwall2004@gmail.com>
2026-04-14 01:24:13 +0530

    Feat: functional component with props

--- Code/src/components/User.tsx
+++ Code/src/components/User.tsx
@@ -3,21 +3,25 @@ import { useEffect, useState } from "react";
 interface UserProps {
     name : string;
 }
-useEffect(() => {
 
-}, []);
 
 
 const User = (props : UserProps) => {
     const [count] = useState(0);
     const [count2] = useState(0);
-    return <div className="user-card">
-        <h1>count= {count}</h1>
-        <h1>count2= {count2}</h1>
-        <h2>Name: {props.name}</h2>
-        <h3>Location: Delhi</h3>
-        <h4>Contact: deepali1234@gmail.com</h4>
-    </div>
+
+    useEffect(() => {
+
+    }, [count , count2]);
+    return (
+        <div className="user-card">
+            <h1>count= {count}</h1>
+            <h1>count2= {count2}</h1>
+            <h2>Name: {props.name}</h2>
+            <h3>Location: Delhi</h3>
+            <h4>Contact: deepali1234@gmail.com</h4>
+        </div>
+    )    
 }
 
 export default User;
