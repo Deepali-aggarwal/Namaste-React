ff95613c3b758305753b7a6d418f4f7d0b96a094
Deepali <deepaggarwall2004@gmail.com>
2026-04-14 01:23:02 +0530

    Feat : Updated Header with navigation links and online status button

--- Code/src/components/Header.tsx
+++ Code/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { LOGO_URL } from "../utils/Constant";
 import { useEffect, useState } from "react";
 import { Link} from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
 
     const [btnNameReact, setbtnNameReact] = useState("Login"); 
+
+    const onlineStatus= useOnlineStatus();
 
     // if no dependency array => useeffect is called on every render
     //if dependency array is empty = [] => useEffect is called on initial render(just once)
@@ -20,12 +23,18 @@ const Header = () => {
             </div>
             <div className = "Nav-items">
                 <ul>
+                    <li>
+                        Online Status : {onlineStatus ? "✅" : "❌"}
+                    </li>
                     <li> <Link to= "/">Home</Link></li>
                     <li>
                         <Link to= "/about">About Us</Link>
                     </li>
                     <li>
                         <Link to="/contact">Contact Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/grocery">Grocery</Link>
                     </li>
                     <li>Cart</li>
                     <button 
