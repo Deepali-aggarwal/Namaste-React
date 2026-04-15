9045bc42919737fa7ee9d744bdb577517aa2b532
Deepali <deepaggarwall2004@gmail.com>
2026-04-16 01:34:34 +0530

    feat: made restaurant category accordian

--- Code/src/utils/useRestaurantMenu.ts
+++ Code/src/utils/useRestaurantMenu.ts
@@ -7,11 +7,21 @@ interface RestaurantInfo {
   cuisines: string[];
   cloudinaryImageId: string;
 }
+interface CategoryCard {
+  card?: {
+    card?: {
+      ["@type"]?: string;
+      title?: string;
+      itemCards?: MenuItemCard[];
+    };
+  };
+}
 
 const useRestaurantMenu = (resId : string) => {
 
     const [resInfo, setResInfo] = useState<RestaurantInfo| null>(null);
-    const [menuItems,setMenuItems] = useState<RestaurantInfo[]>([]);
+    const [menuItems,setMenuItems] = useState<MenuItemCard[]>([]);
+    const [categories, setCategories] = useState<CategoryCard[]>([]);
     useEffect(() => {
         fetchData();
 
@@ -19,10 +29,23 @@ const useRestaurantMenu = (resId : string) => {
     const fetchData = async () => {
         const data = await fetch(MENU_API + resId); 
         const json = await data.json();
+
         setResInfo(json?.data?.cards[2]?.card?.card?.info);
+        
+        const regularCards: CategoryCard[] =json?.data?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards ?? [];
+
+        const items = regularCards
+        ?.filter((c: CategoryCard) => c?.card?.card?.itemCards)
+        .flatMap((c: CategoryCard) => c?.card?.card?.itemCards ?? []);
+
+        setMenuItems(items);
+
+        const filteredCategories = regularCards
+        ?.filter((c:CategoryCard) => c?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+        setCategories(filteredCategories); 
     }
 
-    return {resInfo, menuItems};
+    return {resInfo, menuItems , categories};
 
 }
 
