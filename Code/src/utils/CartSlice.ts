0f69c2cf31f9a2959b074c8998d018eecb8e81ff
Deepali <deepaggarwall2004@gmail.com>
2026-04-24 00:09:14 +0530

    test: setup Jest and Babel for TypeScript and React testing

---
+++ Code/src/utils/CartSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice , PayloadAction} from "@reduxjs/toolkit";
+import { MenuItemCard } from "../types/menu";
+
+interface CartState{
+    items: MenuItemCard[];
+}
+
+const initialState : CartState = {
+    items: [],
+}
+const cartslice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers : {
+        addItem : (state , action : PayloadAction <MenuItemCard>) => {
+
+
+            //Vanilla(older) Redux => DONT MUTATE STATE, returning was mandatory
+            // const newstate = (...state);
+            // newstate.items.push(action.payload);
+            // return newstate;
+
+
+            // mutating the state over
+            state.items.push(action.payload);
+        },
+        removeItem: (state) => {
+            state.items.pop();
+        },
+        clearCart : (state) => {
+        // In redux you can not read the state directly because that will be a proxy object and for reading the original state we need "current"    
+            state.items.length = 0;
+        }
+
+    }
+})
+
+export const {addItem , removeItem , clearCart} = cartslice.actions;
+
+export default cartslice.reducer;
